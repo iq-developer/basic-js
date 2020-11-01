c9eb906c1702135ed19ec440c11df76d5d03f066
Vladimir Sevastianov <108.nimay@gmail.com>
2020-11-01 22:14:13 +0200

    feat: dream team implement

--- src/dream-team.js
+++ src/dream-team.js
@@ -4,10 +4,10 @@ module.exports = function createDreamTeam(members) {
 //function createDreamTeam(members) {
   if (Array.isArray(members)){
     return members.map(function(item){
-      if (typeof item === 'string') return item[0];
+      if (typeof item === 'string') return (item.trim())[0].toUpperCase();
     }).sort().join('');
   }
   return false;
 
 };
-//console.log( createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) );
+//console.log( createDreamTeam(['Olivia', 1111, ' lily', 'Oscar', true, null]) );
