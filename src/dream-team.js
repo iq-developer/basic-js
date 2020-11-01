38d0455e0b32dc13c20cfeeca874a8956334c900
Vladimir Sevastianov <108.nimay@gmail.com>
2020-11-01 22:03:55 +0200

    feat: dream team basics

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+//function createDreamTeam(members) {
+  if (Array.isArray(members)){
+    return members.map(function(item){
+      if (typeof item === 'string') return item[0];
+    }).sort().join('');
+  }
+  return false;
+
 };
+//console.log( createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) );
